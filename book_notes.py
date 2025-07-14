d3eb0b0e0430c6f1857bfb071158e321f07067b4
bearenbey <hi.bearen@pm.me>
2025-07-14 12:21:47 +0200

    new email

--- book_notes.py
+++ book_notes.py
@@ -11,7 +11,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
-# © 2025 Eren Ogrul - iam.bearen@pm.me
+# © 2025 Eren Öğrül - termapp@pm.me
 
 import curses
 import json
